e8e5b4098a1df01b4f5e68ba86ff5d2e89215cef
abiney1321 <abiney1321@gmail.com>
2025-07-11 22:17:39 +0000

    implement offer cards

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,61 @@
-import { Text, View } from "react-native";
-import "./globals.css";
+import { images, offers } from "@/constants";
+import cn from "clsx";
+import { Fragment } from "react";
+import { FlatList, Image, Pressable, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl  text-violet-500 font-quicksand-bold">
-        Welcome to Nativewind!
-      </Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white">
+      <FlatList
+        data={offers}
+        renderItem={({ item, index }) => {
+          const isEven = index % 2 === 0;
+
+          return (
+            <View>
+              <Pressable
+                className={cn(
+                  "offer-card",
+                  isEven ? "flex-row-reverse" : "flex-row"
+                )}
+                style={{ backgroundColor: item.color }}
+                android_ripple={{ color: "#ffffff22" }}
+              >
+                {(presssed) => (
+                  <Fragment>
+                    <View className="h-full w-1/2">
+                      <Image
+                        source={item.image}
+                        className={"size-full"}
+                        resizeMode={"contain"}
+                      />
+                    </View>
+
+                    <View
+                      className={cn(
+                        "offer-card__info",
+                        isEven ? "pl-10" : "pr-10"
+                      )}
+                    >
+                      <Text className="h1-bold text-white leading-tight">
+                        {item.title}
+                      </Text>
+                      <Image
+                        source={images.arrowRight}
+                        className="size-10"
+                        resizeMode="contain"
+                        tintColor="white"
+                      />
+                    </View>
+                  </Fragment>
+                )}
+              </Pressable>
+            </View>
+          );
+        }}
+        contentContainerClassName="pb-28 px-5"
+      />
+    </SafeAreaView>
   );
 }
