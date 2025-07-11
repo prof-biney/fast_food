734fe266a605f494963db56ddc27c64ebf834a05
abiney1321 <abiney1321@gmail.com>
2025-07-11 20:42:18 +0000

    setup font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,21 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 import "./globals.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontLoaded, error] = useFonts({
+    "QuickSand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+    "QuickSand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+    "QuickSand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+    "QuickSand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+    "QuickSand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+    if (fontLoaded) SplashScreen.hideAsync();
+  }, [fontLoaded, error]);
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-bold text-violet-500">
+      <Text className="text-3xl  text-violet-500 font-quicksand-bold">
         Welcome to Nativewind!
       </Text>
     </View>
