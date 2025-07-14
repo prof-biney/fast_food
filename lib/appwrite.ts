9f36063a988c419be530c8e8eb974224ed11a414
abiney1321 <abiney1321@gmail.com>
2025-07-14 21:36:00 +0000

    add qppwrite

---
+++ lib/appwrite.ts
@@ -0,0 +1,5 @@
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  platform: "com.profbiney.fastfood",
+  project: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+};
