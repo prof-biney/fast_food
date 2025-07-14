e686c177955debf84df9d63552f49116ff38f099
abiney1321 <abiney1321@gmail.com>
2025-07-14 21:14:44 +0000

    implement routing

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,10 @@
+import { Redirect, Slot } from "expo-router";
+import React from "react";
+
+export default function _layout() {
+  const isAuthenticated = false; // Replace with actual authentication logic
+
+  if (!isAuthenticated) return <Redirect href="/sign-in" />;
+
+  return <Slot />;
+}
