719cf06c2cb683e842cd5549b38c807af557858d
abiney1321 <abiney1321@gmail.com>
2025-07-14 21:20:39 +0000

    test routing

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,7 +2,7 @@ import { Redirect, Slot } from "expo-router";
 import React from "react";
 
 export default function _layout() {
-  const isAuthenticated = false; // Replace with actual authentication logic
+  const isAuthenticated = true; // Replace with actual authentication logic
 
   if (!isAuthenticated) return <Redirect href="/sign-in" />;
 
