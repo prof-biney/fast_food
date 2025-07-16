6e0d83c1860c278c9359e7a81d2fadcb11de3432
abiney1321 <abiney1321@gmail.com>
2025-07-16 21:43:46 +0000

    setup appwrite

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,5 +1,7 @@
 export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
-  platform: "com.profbiney.fastfood",
   project: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  platform: "com.profbiney.fastfood",
+  databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+  userCollectionId: "687579d00005ff8533cf",
 };
